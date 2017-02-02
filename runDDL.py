8b5dcabe3664d2777d616bdb208c2eeb20f0742f
Unknown <jeg6@hawaii.edu>
2017-02-01 17:32:53 -1000

    Auto stash for cherrypick of "Auto stash for cherrypick of "Initial commit""

--- runDDL.py
+++ runDDL.py
@@ -1,13 +1,27 @@
 import sys
 import mysql.connector
 
-if (len(sys.argv) > 1)
-    sys.argv[2]
+# Allows custom config and ddl files to be run as parameters.
+# clusrcfg and ddlfile are the default if no parameters are entered.
+
+if len(sys.argv) > 1:
+    clustername = sys.argv[1]
+else:
+    clustername = 'clustercfg'
+if len(sys.argv) > 2:
+    ddlname = sys.argv[2]
+else:
+    ddlname = 'ddlfile'
+
+# Opens the config and ddl files
+cluster = open(clustername,'r')
+ddl = open(ddlname,'r')
+
+# Connects to the mysql database
 connect = mysql.connector.connect(user='dbuser',
 password="jesus", host='127.0.0.1',
 database='test')
-
 connect.close()
-cluster = open('clustercfg','r')
-ddl = open('ddlfile','r')
+
+
 print (cluster.read() + ddl.read())
